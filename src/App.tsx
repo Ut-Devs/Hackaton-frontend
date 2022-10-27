be0d99ba8776bed6beb767c41c12f30f9550b8af
Rafael Perina <rafael.prn3@gmail.com>
2022-10-27 14:58:02 -0300

    feat: added content and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+/* eslint-disable indent */
 import './App.css'
 
 function App() {
-
-	return (
-		<div className='content'>
-		</div>
-	)
+  return (
+    <div className="content">
+      <h2 className="title">
+        Aprenda a programar ainda hoje!
+        <p className="subtitle">
+          Com o GrindLearn, você pode aprender os conceitos fundamentais da
+          programação aliados a muita prática, temos aulas do nível básico ao
+          avançado.
+        </p>
+      </h2>
+      <img className="picture" src="https://images.unsplash.com/photo-1643142326340-7a4c55a09745?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
+    </div>
+  )
 }
 
 export default App
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -6,9 +6,13 @@ export const Header = () => {
 	return (
 		<div className="header">
 			<div className="brand">
-				<img src={Logo} className="logo" alt="" />
-                <h1 className="title">GRINDLEARN</h1>
+				<img className="logo" src={Logo} alt="" />
+				<h1 className="title">GRINDLEARN</h1>
 			</div>
+			<div className="botao">
+				<button>ENTRAR</button>
+			</div>
+			{/* <div className="about"></div> */}
 		</div>
 	)
 }
