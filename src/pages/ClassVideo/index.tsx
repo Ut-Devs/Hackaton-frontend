f60535671246f17842f9070f5733004c7e67a44b
jowjow22 <jonathan224santos@gmail.com>
2022-10-30 16:06:24 -0300

    deps: add material ui and create new classes list

--- src/pages/ClassVideo/index.tsx
+++ src/pages/ClassVideo/index.tsx
@@ -2,18 +2,30 @@ import { useEffect, useState } from 'react'
 import styles from './styles.module.scss'
 import ReactPlayer from 'react-player'
 import api from '../../services/baseUrl'
+import { Collapse, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import {ExpandLess, ExpandMore, StarBorder, RadioButtonUncheckedOutlined, OndemandVideo } from '@mui/icons-material'
+
 
 const ClassVIdeo = () => {
+	const [open, setOpen] = useState<{open: boolean, index: number}>({open: true, index: 0})
 	const [course, setCourse] = useState<any>()
 	const [videoUrl, setVideoUrl] = useState<string>()
 	const [videoTitle, setVideoTitle] = useState<string>()
 
 	const getClasses = async () => {
 		const response = await api.get('/classes')
-		console.log(response.data)
 		setVideoUrl(response.data.modules[0].classes[0].link)
 		setVideoTitle(response.data.modules[0].classes[0].name)
 		setCourse(response.data)
+	}
+
+	const handleClick = (index: number) => {
+		if(open.index == index && open.open == true) {
+			setOpen({open: false, index})
+		}
+		else{
+			setOpen({open: true, index: index})
+		}
 	}
 
 	useEffect(() => {
@@ -28,31 +40,38 @@ const ClassVIdeo = () => {
 				<h2 className={styles.title}>{videoTitle}</h2>
 				<ReactPlayer url={videoUrl} width="100%" height="90%" />
 			</div>
-			<div className={styles.description}>
-				<div className={styles.modules__list}>
-					<ul className={styles.modules}>
-						{course?.modules.map((module: any, index: number) => (
-							<li key={index} className={styles.module__container} onClick={(e) => {
-								e.target.style.maxHeight == '100%' ? e.target.style.maxHeight = '20px' : e.target.style.maxHeight = '100%'
+			<div className={styles.modules__list}>
+				{
+					course?.modules.map((moduleClass: any, index: number) => (
+						<List key={index}>
+							<ListItemButton onClick={() => {
+								handleClick(index)
 							}}>
-								{module.module_name}
-								<ul className={styles.subitem}>
-									{module.classes.map((classs: any, index: number) => (
-										<li key={index} className={styles.classPointerName}>
-											<a onClick={() =>{
-												setVideoUrl(classs.link)
-												setVideoTitle(classs.name)
+								<ListItemIcon>
+									<RadioButtonUncheckedOutlined />
+								</ListItemIcon>
+								<ListItemText primary={moduleClass.module_name} />
+								{open ? <ExpandLess /> : <ExpandMore />}
+							</ListItemButton>
+							<Collapse in={open.open && open.index == index} timeout="auto" unmountOnExit>
+								<List component="div" disablePadding>
+									{
+										moduleClass.classes.map((classItem: any, index: number) => (
+											<ListItemButton key={index} sx={{ pl: 4 }} onClick={() => {
+												setVideoUrl(classItem.link)
 											}}>
-												{classs.name}
-											</a>
-										</li>
-									))}
-
-								</ul>
-							</li>
-						))}
-					</ul>
-				</div>
+												<ListItemIcon>
+													<OndemandVideo />
+												</ListItemIcon>
+												<ListItemText primary={classItem.name} />
+											</ListItemButton>
+										))
+									}
+								</List>
+							</Collapse>
+						</List>
+					))
+				}
 			</div>
 		</div>
 	)
